c86cdec56be216853d5b0a48fb0671111a0adc21
Jayavardhan <jayavardhan.work@gmail.com>
2022-10-19 14:26:06 +0530

    first commit

---
+++ src/main/java/org/example/sample/Hello.java
@@ -0,0 +1,10 @@
+package org.example.sample;
+
+import java.util.Arrays;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println();
+    }
+}
